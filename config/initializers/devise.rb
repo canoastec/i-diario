a1c362f55e297eb3eaaa4063b29b4dd19934fe6c
Lucas <lucascsvet@gmail.com>
2026-05-13 16:01:18 -0300

    #0000 - email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   # config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.mailer_sender = Rails.application.secrets.NO_REPLY_ADDRESS || 'norepy@canoasedu.rs.gov.br'
+  config.mailer_sender = Rails.application.secrets.NO_REPLY_ADDRESS || 'noreply@canoasedu.rs.gov.br'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
